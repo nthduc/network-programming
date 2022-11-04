59f33a3e3db4eee9af99eaa74a8c644038446fcf
nthduc <nguyenthaiduc0212@gmail.com>
2022-11-04 22:46:32 +0700

    on tap giua ky

---
+++ ontapgiuaky/Pack_UnPack02.java
@@ -0,0 +1,76 @@
+package ontapgiuaky;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+public class Pack_UnPack02 {
+	public void pack(String sFolder, String destFile) throws IOException {
+		File folder = new File(sFolder);
+		if(!folder.exists()) return;
+		
+		File[] listFiles = folder.listFiles();
+		int size = listFiles.length;
+		RandomAccessFile raf = new RandomAccessFile(destFile, "rw");
+		raf.writeInt(size);
+		ArrayList<Long> positions = new ArrayList<Long>();
+		for (File file : listFiles) {
+			raf.writeUTF(file.getName());
+			raf.writeLong(file.length());
+			positions.add(raf.getFilePointer());
+			raf.writeLong(0);
+		}
+		
+		int i = 0;
+		for (File file : listFiles) {
+			long positionFile = raf.getFilePointer();
+			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+			
+			byte[] buffer = new byte[1024];
+			int byteReader;
+			while((byteReader = bis.read(buffer)) != -1) {
+				raf.write(buffer,0,byteReader);
+			}
+			bis.close();
+			raf.seek(positions.get(i));
+			raf.writeLong(positionFile);
+			raf.seek(raf.length());
+			i++;
+			
+		}
+		
+		raf.close();
+	}
+	
+	public void unpack(String sFile, String filename) throws IOException {
+		File fileSource = new File(sFile);
+		if(!fileSource.exists()) return;
+		String parent = fileSource.getParent();
+		
+		RandomAccessFile raf = new RandomAccessFile(fileSource, "rw");
+		int totalFile = raf.readInt();
+		for(int i = 0 ; i < totalFile; i++) {
+			String name = raf.readUTF();
+			Long size = raf.readLong();
+			Long positions = raf.readLong();
+			
+			if(filename.equals(name)) {
+				raf.seek(positions);
+				FileOutputStream fos = new FileOutputStream(parent + "unpack"+ filename);
+				for(int j = 0 ; j <= size; j++) {
+					fos.write(raf.read());
+				}
+				
+				fos.close();
+				break;
+			}
+			raf.close();
+			
+		}
+	}
+}
---
+++ ontapgiuaky/Split_JoinFile03.java
@@ -0,0 +1,66 @@
+package ontapgiuaky;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Split_JoinFile03 {
+	public void splitFile(String path, long size) throws IOException {
+		File fileSource = new File(path);
+		if(!fileSource.exists()) return;
+		if(fileSource.isFile()){
+			FileInputStream fis = new FileInputStream(fileSource);
+			long totalSize = fileSource.length();
+			int countFile = (int) (totalSize / size);
+			long remainder = totalSize % size;
+			System.out.println(totalSize);
+			System.out.println(countFile);
+			System.out.println(remainder);
+			File newFile = new File(fileSource.getParent() + "\\split");
+			newFile.mkdir();
+			
+			for(int i = 0; i < countFile; i++) {
+				FileOutputStream fos = new FileOutputStream(fileSource.getParent() + "\\split\\split" + (i+1));
+				for(int j = 0 ; j < size; j++) {
+					fos.write(fis.read());
+				}
+				fos.close();
+			}
+			
+			if(remainder > 0) {
+				FileOutputStream fos = new FileOutputStream(fileSource.getParent() + "\\split_end");
+				for(int i = 0 ; i < remainder; i++) {
+					fos.write(fis.read());
+					
+				}
+				fos.close();
+			}
+			
+		}
+	}
+	
+	public void joinFile(String pathFolder , String ext) throws IOException {
+		File folder = new File(pathFolder);
+		if(!folder.exists()) return;
+		if(folder.isDirectory()) {
+			File[] listFiles = folder.listFiles();
+			FileOutputStream fos = new FileOutputStream(folder.getParent() + "\\Joiner" + ext);
+			for(int i = 0 ; i < listFiles.length; i++) {
+				FileInputStream fis;
+				if(i == listFiles.length - 1) {
+					fis = new FileInputStream(folder.getParent() + "\\split_end");
+					
+				} else {
+					fis = new FileInputStream(folder.getParent() + "\\split" + (i+1));
+				}
+				
+				int byteReader;
+				while((byteReader = fis.read()) != -1) {
+					fos.write(byteReader);
+				}
+				fis.close();
+			}
+		}
+	}
+}
