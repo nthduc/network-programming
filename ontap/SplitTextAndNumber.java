3efd59bf1a7203d86fc9c01390021a68563bc89e
nthduc <nguyenthaiduc0212@gmail.com>
2022-11-05 07:40:29 +0700

    a

--- ontap/SplitTextAndNumber.java
+++ ontap/SplitTextAndNumber.java
@@ -13,36 +13,50 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 public class SplitTextAndNumber {
-	public int splitTextAndNumber(String srcFile, String wordFile, String number) throws IOException, FileNotFoundException {
+	public int splitTextAndNumber(String srcFile, String wordFile, String numFile) throws IOException {
 		File src = new File(srcFile);
-		if(!src.exists()) return -1;
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(src),"UTF-8"));
-		PrintWriter wordText = new PrintWriter(new OutputStreamWriter(new FileOutputStream(src),"UTF-8"));
-		PrintWriter numberWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(number),"UTF-8"));
+		if (!src.exists())
+			return -1;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(src), "UTF-16"));
+		PrintWriter wordWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(wordFile), "UTF-8"));
+		PrintWriter numWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(numFile), "UTF-8"));
 		String line;
 		int count = 0;
-		while((line = br.readLine()) != null) {
+		while ((line = reader.readLine()) != null) {
 			String numberLine = "";
 			String wordLine = "";
 			System.out.println(line);
 			String[] split = line.split(" ");
-			for(int i = 0 ; i < split.length; i++) {
-				if(isNumberic(split[i])) {
+			for (int i = 0; i < split.length; i++) {
+				if (isNumeric(split[i])) {
 					numberLine += split[i] + " ";
 					count++;
 				} else {
 					wordLine += split[i] + " ";
 				}
 			}
-			wordText.println(wordLine);
-			numberWriter.println(number)
+			wordWriter.println(wordLine);
+			numWriter.println(numberLine);
 		}
-		
+		reader.close();
+		wordWriter.close();
+		numWriter.close();
+		return count;
 	}
 
-	private boolean isNumberic(String string) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isNumeric(String s) {
+		try {
+			Double.parseDouble(s);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
-}
+	public static void main(String[] args) throws IOException {
+		String srcFile="D:\\TestLapTrinhMang\\testSplitNumberAndFile.txt";
+		String wordFile="D:\\TestLapTrinhMang\\wordFile.txt";
+		String numFile="D:\\TestLapTrinhMang\\numFile.txt";
+		BT2 bt2 = new BT2();
+		System.out.println(bt2.splitTextAndNumber(srcFile, wordFile, numFile));
+		
+	}
